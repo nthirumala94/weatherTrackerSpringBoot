9f084c7ac80adb33325abf1ae70f358d368c802a
HackerRank <hackerrank@example.com>
2018-12-08 21:14:19 +0000

    Saved at Sat Dec 08 2018 16:14:18 GMT-0500 (Eastern Standard Time)

--- src/main/java/com/capitalone/weathertracker/measurements/MeasurementsResource.java
+++ src/main/java/com/capitalone/weathertracker/measurements/MeasurementsResource.java
@@ -29,7 +29,7 @@ public class MeasurementsResource {
 	  System.out.println("Inside Post");
 	  store.add(measurement);
 
-	  return null;
+	  return null
   }
 
   // features/01-measurements/02-get-measurement.feature
